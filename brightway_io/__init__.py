a01ac91612e9cb87788d5cd449ec1215d79c4373
Chris Mutel <cmutel@gmail.com>
2019-10-07 09:16:34 +0200

    First checkin

---
+++ brightway_io/__init__.py
@@ -0,0 +1,68 @@
+# __all__ = [
+#     'activity_hash',
+#     'add_ecoinvent_33_biosphere_flows',
+#     'add_ecoinvent_34_biosphere_flows',
+#     'add_ecoinvent_35_biosphere_flows',
+#     'bw2setup',
+#     'create_core_migrations',
+#     'create_default_biosphere3',
+#     'create_default_lcia_methods',
+#     'CSVImporter',
+#     'DatabaseSelectionToGEXF',
+#     'DatabaseToGEXF',
+#     'Ecospold1LCIAImporter',
+#     'es2_activity_hash',
+#     'ExcelImporter',
+#     'get_csv_example_filepath',
+#     'get_xlsx_example_filepath',
+#     'lci_matrices_to_excel',
+#     'lci_matrices_to_matlab',
+#     'load_json_data_file',
+#     'Migration',
+#     'migrations',
+#     'MultiOutputEcospold1Importer',
+#     'normalize_units',
+#     'SimaProCSVImporter',
+#     'SimaProLCIACSVImporter',
+#     'SingleOutputEcospold1Importer',
+#     'SingleOutputEcospold2Importer',
+#     'unlinked_data',
+#     'UnlinkedData',
+# ]
+
+__version__ = (3, 0, "dev")
+
+# from .export import (
+#     DatabaseToGEXF, DatabaseSelectionToGEXF, keyword_to_gephi_graph,
+#     lci_matrices_to_excel,
+#     lci_matrices_to_matlab,
+# )
+# from .data import (
+#     add_ecoinvent_33_biosphere_flows,
+#     add_ecoinvent_34_biosphere_flows,
+#     add_ecoinvent_35_biosphere_flows,
+#     get_csv_example_filepath,
+#     get_xlsx_example_filepath,
+# )
+# from .migrations import migrations, Migration, create_core_migrations
+# from .importers import (
+#     CSVImporter,
+#     Ecospold1LCIAImporter,
+#     ExcelImporter,
+#     MultiOutputEcospold1Importer,
+#     SimaProCSVImporter,
+#     SimaProLCIACSVImporter,
+#     SingleOutputEcospold1Importer,
+#     SingleOutputEcospold2Importer,
+# )
+# from .units import normalize_units
+# from .unlinked_data import unlinked_data, UnlinkedData
+# from .utils import activity_hash, es2_activity_hash, load_json_data_file
+
+# def create_default_lcia_methods(overwrite=False, rationalize_method_names=False):
+#     from .importers import EcoinventLCIAImporter
+#     ei = EcoinventLCIAImporter()
+#     if rationalize_method_names:
+#         ei.add_rationalize_method_names_strategy()
+#     ei.apply_strategies()
+#     ei.write_methods(overwrite=overwrite)
